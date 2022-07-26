ddb5d9a11b0cdfc210056f8897bd3c689b39cefa
bungee <nikola.banek.kc2@gmail.com>
2022-07-26 20:05:36 +0100

    Tweets now fetched every minute and 40 seconds

--- src/twitter.py
+++ src/twitter.py
@@ -11,7 +11,7 @@ from storageMethods import save_twitter_accounts, load_twitter_accounts, load_sc
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAHYFOQEAAAAAotRfiWhRaZJG2bnG%2BUwv2d7ebho%3Dt6y5qpIBZ7aGcivxrmO1StTpjvR1ozBypmRYbQKxiJ6Vo3VIs8'
+#bearer_token = 'AAAAAAAAAAAAAAAAAAAAAHYFOQEAAAAAotRfiWhRaZJG2bnG%2BUwv2d7ebho%3Dt6y5qpIBZ7aGcivxrmO1StTpjvR1ozBypmRYbQKxiJ6Vo3VIs8'
 
 def create_url(user_id):
     return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
@@ -47,11 +47,11 @@ def get_tweets(user_id, since='', attempt = 0):
     try:
         json_res = connect_to_endpoint(url, params)
         return json_res
-    except (requests.RequestException, requests.ConnectionError, requests.Timeout) as err:
+    except (requests.RequestException, requests.ConnectionError, requests.Timeout, Exception) as err:
         if type(err) == requests.ConnectionError and attempt < 5:
             attempt += 1
             logging.info(f'Connection was closed, attempting to fetch tweets again. Attempt: {attempt}.')
-            get_tweets(user_id, since, attempt)
+            return get_tweets(user_id, since, attempt)
         else:
             logging.error(f'Error fetching tweets for {user_id}:\n{err}')
 
@@ -79,10 +79,19 @@ def process_tweets():
 
 def twitter():
     logging.info('Starting twitter terminal')
-    accounts = load_twitter_accounts()
     global initialized
+    global bearer_token
+    bearer_tokens = ['AAAAAAAAAAAAAAAAAAAAAHYFOQEAAAAAotRfiWhRaZJG2bnG%2BUwv2d7ebho%3Dt6y5qpIBZ7aGcivxrmO1StTpjvR1ozBypmRYbQKxiJ6Vo3VIs8', 'AAAAAAAAAAAAAAAAAAAAANy9fAEAAAAAXcqpQ8Yt9irxX8uPljRnQT4GBsY%3DwgNEzmk9huZh55bkFRW8y8MFtkzlLsRqWNt3wKyxjQ538NiLPl', 'AAAAAAAAAAAAAAAAAAAAAMu9fAEAAAAA0GROl01yTH9brD2vJqKsEnaebFs%3DSJ3DzryXaQF6dNbK6tdI3hAB5QpZ2sskQEW2BbbTYDxD5tEPnI']
     initialized = False
+    accounts = load_twitter_accounts()
+    bearer_count = 0
     while(True):
+        if bearer_count < 3: bearer_token = bearer_tokens[0]
+        if bearer_count < 6 and bearer_count >= 3: bearer_token = bearer_tokens[1]
+        if bearer_count < 9 and bearer_count >= 6:bearer_token = bearer_tokens[2]
+        if bearer_count >= 9: 
+            bearer_token = bearer_tokens[0]
+            bearer_count = 0
         global q 
         q = Queue(len(accounts))
         for a in accounts:
@@ -96,10 +105,12 @@ def twitter():
         except Exception as ex:
             print(ex)
         if not initialized: initialized = True
-        #save the most recent tweet information to file
         logging.info('Saving new twitter information')
         save_twitter_accounts(accounts)
-        sleep(360)
+        bearer_count += 1
+        print(f'BEARER TOKEN USE COUNT: {bearer_count}')
+        print(f'TOKEN: {bearer_token}')
+        sleep(100)
 
 def get_tweets_from_single_account():
     a = {"user_id": "902839045356744704", "latest_tweet": "1550413482725900288", "username": "justinsuntron"}
@@ -154,4 +165,4 @@ def old():
         if not initialized: initialized = True
         #save the most recent tweet information to file
         save_twitter_accounts(accounts)
-        sleep(360)
+        sleep(140)
