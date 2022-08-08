0ada1cbaac6021bd92b5c0315072e35c33b1f7c3
bungee <nikola.banek.kc2@gmail.com>
2022-08-08 20:50:25 +0100

    Added function for fetching listed coins on gateio

--- src/gatePI.py
+++ src/gatePI.py
@@ -1,3 +1,4 @@
+import re
 import gate_api
 from gate_api.exceptions import ApiException, GateApiException
 
@@ -7,22 +8,21 @@ configuration = gate_api.Configuration(
     host = "https://api.gateio.ws/api/v4"
 )
 
-
 api_client = gate_api.ApiClient(configuration)
 # Create an instance of the API class
 api_instance = gate_api.SpotApi(api_client)
 
-try:
-    # List all currencies' details
-    api_response = api_instance.list_currencies()
-    # for coin in api_response:
-    #     print(coin['currency'])
-    with open('api_response.txt', 'w') as f:
+def get_gateio_listed_coins():
+    try:
+        # List all currencies' details
+        api_response = api_instance.list_currencies()
+        coins = []
         for curr in api_response:
-            f.write(curr.currency)
-            f.write('\n')
-
-except GateApiException as ex:
-    print("Gate api exception, label: %s, message: %s\n" % (ex.label, ex.message))
-except ApiException as e:
-    print("Exception when calling SpotApi->list_currencies: %s\n" % e)
+            if '_' not in curr.currency and '3' not in curr.currency and '5' not in curr.currency:
+                coin = re.sub("[^A-Za-z]","", curr.currency)
+                coins.append(coin)
+        return coins
+    except GateApiException as ex:
+        print("Gate api exception, label: %s, message: %s\n" % (ex.label, ex.message))
+    except ApiException as e:
+        print("Exception when calling SpotApi->list_currencies: %s\n" % e)
