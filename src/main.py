21c197d44202a2069a7d507b0d5e5bc6affc287a
bungee <nikola.banek.kc2@gmail.com>
2022-09-17 17:12:21 +0100

    .

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from futures import check_for_futures_updates
 
 def main():
     #filename='gateWebsocketLog.txt',filemode='a',
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     g = threading.Thread(target=gateio)
     m = threading.Thread(target=mexc)
     k = threading.Thread(target=kucoin)
