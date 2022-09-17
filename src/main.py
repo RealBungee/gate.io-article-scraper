f88cbb8467b0e720183d28ae8d85377d824f6b68
bungee <nikola.banek.kc2@gmail.com>
2022-09-17 16:42:55 +0100

    Refactored main

--- src/main.py
+++ src/main.py
@@ -1,23 +1,13 @@
 import logging
 import threading
-from time import sleep
 from mexc import mexc
 from kucoin import kucoin, start_kucoin_websocket
 from gate import gateio, start_gateio_websocket
-from storageMethods import update_futures_listings
-
-def check_for_futures_updates():
-    logging.info('Futures thread started')
-    while(True):
-        try:
-            logging.info('Checking for futures updates')
-            update_futures_listings()
-            sleep(60)
-        except TypeError as err:
-            logging.error(f'Error checking for listings: {err}')
+from futures import check_for_futures_updates
 
 def main():
-    logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    #filename='gateWebsocketLog.txt',filemode='a',
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(threadName)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     g = threading.Thread(target=gateio)
     m = threading.Thread(target=mexc)
     k = threading.Thread(target=kucoin)
@@ -26,11 +16,11 @@ def main():
     futures = threading.Thread(target=check_for_futures_updates)
 
     logging.info('Starting threads')
-    g.start()
-    k.start()
+    #g.start()
+    #k.start()
     m.start()
-    gw.start()
-    kw.start()
-    futures.start()
+    #gw.start()
+    #kw.start()
+    #futures.start()
     
 main()
