ac7e87f27d3788dc1fdad17a777a289f738210a7
RealBungee <>
2022-09-22 22:23:58 +0100

    Small fix, forgot to select list from response

--- src/mexcAPI.py
+++ src/mexcAPI.py
@@ -22,9 +22,9 @@ def load_list(listName):
 
 def get_listed_tokens():
     url = 'https://api.mexc.com/api/v3/exchangeInfo'
-    res = requests.get(url = url).json()
+    res = requests.get(url = url).json().get('symbols')
     return res  
-
+    
 def filter_list_to_usdt_pairs(coin_list):
     list_of_substrings = ['2L', '2S', '3S', '3L', '4L', '4S', '5L', '5S']
     coin_list = list(filter(lambda coin: coin['quoteAsset']=='USDT', coin_list))
